ce3d5bd7b80864ba88306795a341f3615c8012b7
arjunloll <lallarjun@gmail.com>
2016-10-26 02:05:53 -0400

    ship on grey background

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """A class storing all settings for alien invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        #Screen settings
+        self.screen_width = 600
+        self.screen_height= 450
+        self.bg_color = (230,230,230)
