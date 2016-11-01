80b86bb5ecc0af8d7e45c752e5c77e823206f371
arjunloll <lallarjun@gmail.com>
2016-10-31 23:48:44 -0400

    move right

--- game_functions.py
+++ game_functions.py
@@ -2,13 +2,18 @@ import sys
 
 import pygame
 
-def check_events():
+def check_events(ship):
     """Respond to keypresses and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-def update_screen(ai.settings, screen, ship):
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                #Move ship to right
+                ship.rect.centerx += 1
+
+def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to new screen"""
     #Redraw screen during each pass through loop
     screen.fill(ai_settings.bg_color)
